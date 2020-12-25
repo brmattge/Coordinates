3b37c86f1baa3135e5d3569dab4e377e63f6e393
Bruno Mattge <brmattge@gmail.com>
2020-12-25 06:41:42 -0300

    Commit

---
+++ Coordinates/CoordenadasModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coordinates
+{
+    class CoordenadasModel
+    {
+        public Coordenadas coord { get; set; }
+    }
+
+    class Coordenadas
+    {
+        public string lat { get; set; }
+        public string lon { get; set; }
+    }
+}
---
+++ Coordinates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace Coordinates
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string appId = "SEU ID AQUI";
+
+            Console.WriteLine("Digite uma cidade: ");
+            string cidade = Console.ReadLine();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/weather");
+                client.DefaultRequestHeaders.Accept.Clear();
+                HttpResponseMessage respon = await client.GetAsync("?q=" + cidade + "&APPID=" + appId + "&units=metric");
+
+                if (respon.StatusCode == HttpStatusCode.OK)
+                {
+                    CoordenadasModel c = respon.Content.ReadAsAsync<CoordenadasModel>().Result;
+                    Console.WriteLine("Latitude: " + c.coord.lat);
+                    Console.WriteLine("Longitude: " + c.coord.lon);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
